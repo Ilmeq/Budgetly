796509fe1e8cb1ed430db66c602fd0b91ce079e2
Ilmeq <67201202+Ilmeq@users.noreply.github.com>
2025-05-25 21:42:36 +0200

    Delete client directory

--- client/src/pages/AddIncomePage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AddIncomeForm from "../components/AddIncomeForm";
-
-const AddIncomePage = () => {
-  return <AddIncomeForm />;
-};
-
-export default AddIncomePage;
